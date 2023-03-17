f3dec6a1b28eeb1658775c7a10d803134cb74d60
Owen Kaluza <owen.kaluza@monash.edu>
2023-03-17 15:43:54 +1100

    Docs rebuilt for release

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.36',
+    VERSION: '1.8.40',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
